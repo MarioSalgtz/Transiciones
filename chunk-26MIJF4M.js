589cf04de725334e05b9f031aa421e60227d2f51
Mario Salgtz <mario.salcidogtz@gmail.com>
2024-11-03 23:50:28 -0600

    Auto-generated commit

---
+++ chunk-26MIJF4M.js
@@ -0,0 +1,1 @@
+import{Na as o,Oa as a,W as t,Wa as i,bb as m}from"./chunk-AAGVWEFL.js";var s=class n{static \u0275fac=function(e){return new(e||n)};static \u0275cmp=t({type:n,selectors:[["app-admin"]],standalone:!0,features:[m],decls:2,vars:0,template:function(e,r){e&1&&(o(0,"p"),i(1,"admin works!"),a())}})};export{s as AdminComponent};
